a943beb4593c753663fac5e6418a2379654d9f50
Santiago Ramirez <santi_coding@outlook.com>
2021-07-28 22:11:22 -0400

    just updating

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,17 @@
 import React from "react";
 import "./styles/Header.css";
 
+//Components
+import Logo from "./Logo";
+import Navbar from "./Navbar";
+
+//images
+import theLogo from "../assets/images/svg/earth.svg";
+
 function Header() {
   return (
     <div className="Header">
-      <Logo />
+      <Logo title="Country Data 2.0" theLogo={theLogo} />
       <Navbar />
     </div>
   );
