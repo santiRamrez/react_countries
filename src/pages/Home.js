2a44ff92ed35579d78fec95d9e027be7e58c3f80
Santiago Ramirez <santi_coding@outlook.com>
2021-08-02 04:30:01 -0400

    I achived to fetch the data from the API

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,30 +3,14 @@ import React from "react";
 //Components
 import Header from "../components/Header";
 import Intro from "../components/Intro";
-import Countries from "../components/Countries";
+import BoardCountries from "../components/BoardCountries";
 
 function Home() {
   return (
     <div className="Home-page">
       <Header />
       <Intro intro="Search for a country's data" />
-      <div className="board-countries">
-        <Countries />
-        <Countries />
-        <Countries />
-        <Countries />
-        <Countries />
-        <Countries />
-        <Countries />
-        <Countries />
-        <Countries />
-        <Countries />
-        <Countries />
-        <Countries />
-        <Countries />
-        <Countries />
-        <Countries />
-      </div>
+      <BoardCountries />
     </div>
   );
 }
