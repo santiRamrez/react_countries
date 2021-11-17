8ca7ebe37de2befda5555aa759b2d85f85acb616
Santiago Ramirez <santi_coding@outlook.com>
2021-11-16 23:55:43 -0300

    Starting to install chart js but with a bunch of problems

---
+++ src/components/TheChart.js
@@ -0,0 +1,93 @@
+import React from "react";
+import "./styles/TheChart.css";
+
+// //To use an external library, in this case Chart.js
+// import ScriptTag from "react-script-tag";
+
+import {
+  Chart,
+  ArcElement,
+  LineElement,
+  BarElement,
+  PointElement,
+  BarController,
+  BubbleController,
+  DoughnutController,
+  LineController,
+  PieController,
+  PolarAreaController,
+  RadarController,
+  ScatterController,
+  CategoryScale,
+  LinearScale,
+  LogarithmicScale,
+  RadialLinearScale,
+  TimeScale,
+  TimeSeriesScale,
+  Decimation,
+  Filler,
+  Legend,
+  Title,
+  Tooltip,
+  SubTitle,
+} from "chart.js";
+
+Chart.register(
+  ArcElement,
+  LineElement,
+  BarElement,
+  PointElement,
+  BarController,
+  BubbleController,
+  DoughnutController,
+  LineController,
+  PieController,
+  PolarAreaController,
+  RadarController,
+  ScatterController,
+  CategoryScale,
+  LinearScale,
+  LogarithmicScale,
+  RadialLinearScale,
+  TimeScale,
+  TimeSeriesScale,
+  Decimation,
+  Filler,
+  Legend,
+  Title,
+  Tooltip,
+  SubTitle
+);
+
+//Component here!!
+const TheChart = () => {
+  //Data to display within the chart
+  const labels = ["January", "February", "March", "April", "May", "June"];
+  const data = {
+    labels: labels,
+    datasets: [
+      {
+        label: "My First dataset",
+        backgroundColor: "rgb(255, 99, 132)",
+        borderColor: "rgb(255, 99, 132)",
+        data: [0, 10, 5, 2, 20, 30, 45],
+      },
+    ],
+  };
+  //Configuration Chart js
+  const config = {
+    type: "line",
+    data: data,
+    options: {},
+  };
+
+  const myChart = new Chart(document.getElementById("myChart"), config);
+
+  return (
+    <div className="theChart-container">
+      <canvas id="myChart"></canvas>
+    </div>
+  );
+};
+
+export default TheChart;
--- src/pages/Charts.js
+++ src/pages/Charts.js
@@ -4,6 +4,7 @@ import "./styles/ChartsPage.css";
 //Components
 import SidebarFilter from "../components/SidebarFilter";
 import CountriesSelected from "../components/CountriesSelected";
+import TheChart from "../components/TheChart";
 
 function Charts({ dataAutocomplete }) {
   const [data, setReload] = useState(dataAutocomplete);
@@ -11,8 +12,13 @@ function Charts({ dataAutocomplete }) {
     <div className="chartPage">
       <h1>Look and Compare</h1>
       <div className="containerComponts">
-        <SidebarFilter listCountries={dataAutocomplete} />
-        <CountriesSelected />
+        <div className="sideBar-container">
+          <SidebarFilter listCountries={dataAutocomplete} />
+        </div>
+        <div className="mainSection-container">
+          <CountriesSelected />
+          <TheChart />
+        </div>
       </div>
     </div>
   );
