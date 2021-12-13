1fe359d3217ae5ada6b666aaf18f5e62cedb6e10
Santiago Ramirez <santi_coding@outlook.com>
2021-12-13 20:35:24 -0300

    Updating

---
+++ src/components/Maps.js
@@ -0,0 +1,12 @@
+import React from "react";
+// import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+
+import "./styles/Maps.css";
+
+const Maps = () => {
+  // create map
+
+  return <div>Hola</div>;
+};
+
+export default Maps;
--- src/pages/Country.js
+++ src/pages/Country.js
@@ -3,95 +3,89 @@ import "./styles/CountryPage.css";
 import { useParams } from "react-router-dom";
 
 import Loader from "../components/Loader";
+import Maps from "../components/Maps";
 import GetData from "../utils/GetData";
 import FormatCurrency from "../utils/FormatCurrency";
 import formatingNumbers from "../utils/formatingNumbers";
 
-function Country() {
+//Page
+import Error404 from "./404";
+
+function Country({ list }) {
   const [data, setData] = useState({});
   const [load, setLoad] = useState(false);
   let { id } = useParams();
+  const country = list.includes(id) ? id : "There is not such a country";
 
   useEffect(() => {
+    if (country === "There is not such a country") return false;
     const fetchData = async () => {
       const [dataCountry] = await GetData(id);
       setData(dataCountry);
     };
     fetchData();
-    setLoad(true);
+    fetchData().then(() => {
+      setLoad(true);
+    });
   }, []);
 
   const content = () => {
-    if (data) {
-      return (
-        <ul>
-          <li>
-            Capital:<span>{data.capital}</span>
-          </li>
-          <li>
-            Population:
-            <span>
-              {data.population
-                ? formatingNumbers(data.population)
-                : "...loading"}
-            </span>
-          </li>
-          <li>
-            Region:<span>{data.region}</span>
-          </li>
-          <li>
-            Area:
-            <span>
-              {data.area ? formatingNumbers(data.area) : "...loading"} Km2
-            </span>
-          </li>
-          <li>
-            Currency:
-            <span>
-              {data.currencies
-                ? FormatCurrency(data.currencies)[0]
-                : "...loading"}
-            </span>
-            <span>
-              {data.currencies
-                ? FormatCurrency(data.currencies)[1]
-                : "...loading"}
-            </span>
-          </li>
-        </ul>
-      );
-    } else {
-      <h1>No data</h1>;
-    }
+    return (
+      <ul>
+        <li>
+          Capital:<span>{data.capital}</span>
+        </li>
+        <li>
+          Population:
+          <span>
+            {data.population ? formatingNumbers(data.population) : "...loading"}
+          </span>
+        </li>
+        <li>
+          Region:<span>{data.region}</span>
+        </li>
+        <li>
+          Area:
+          <span>
+            {data.area ? formatingNumbers(data.area) : "...loading"} Km2
+          </span>
+        </li>
+        <li>
+          Currency:
+          <span>
+            {data.currencies
+              ? FormatCurrency(data.currencies)[0]
+              : "...loading"}
+          </span>
+          <span>
+            {data.currencies
+              ? FormatCurrency(data.currencies)[1]
+              : "...loading"}
+          </span>
+        </li>
+      </ul>
+    );
   };
 
-  const mainImage = () => {
-    if (data) {
-      return (
-        <img
-          src={data.flags.svg ? data.flags.svg : "https://flagcdn.com/pe.svg"}
-        />
-      );
-    }
-  };
-
-  return (
-    <div className="countryPage-container">
-      <h1>{id}</h1>
-      {!load && <Loader />}
-      <div className="dataCountry-container">
-        <div className="flagContainer">
-          <div className="flagHere">
-            <img
-              src={load ? data.flags.svg : "https://flagcdn.com/pe.svg"}
-              alt={id}
-            />
+  const loadPage = () => {
+    return (
+      <div className="countryPage-container">
+        <h1>{country}</h1>
+        {!load && <Loader />}
+        <div className="dataCountry-container">
+          <div className="flagContainer">
+            <div className="flagHere">
+              <img src={load ? data.flags.svg : "...Loading"} alt={country} />
+            </div>
           </div>
+          <div className="dataContainer">{load ? content() : null}</div>
         </div>
-        <div className="dataContainer">{load ? content() : null}</div>
+        <div className="mapContainer"></div>
       </div>
-    </div>
-  );
+    );
+  };
+
+  return <>{list.includes(id) ? loadPage() : <Error404 />}</>;
 }
 
 export default Country;
